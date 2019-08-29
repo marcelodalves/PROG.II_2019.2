be5e738b063be753af7b12218b786836b625b249
marcelodalves <43051943+marcelodalves@users.noreply.github.com>
2019-08-29 20:01:01 -0300

    Create exerciceFUNCTIONsimetrica.c

---
+++ exerciceFUNCTIONsimetrica.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+void lermatriz(); 
+int simetrica();
+void mostra();
+
+int main(){
+
+	lermatriz();
+	int sim = simetrica();
+	if (sim==1){
+		printf("\nsimetrica");
+	}
+	else{
+		printf("\n nao simetrica");
+	}	
+	
+	
+	
+	
+	return 0;
+}
+
+void lermatriz(){
+	int i,j, mat[3][3];
+	for (i=0;i<3;i++){
+		for (j=0;j<3;j++){
+			printf("\nEntre com a matriz [%d] [%d]",i,j);
+			scanf("%d",&mat[i][j]);
+		}
+	}
+}
+
+int simetrica(){
+	int i, j, mat[3][3];
+	for (i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			if (mat[i][j]!=mat[j][i]){
+				return 0;
+			}
+		}
+	}
+		return 1;
+}
